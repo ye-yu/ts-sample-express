97300d4e9d158e6eec3d1e835327dec04f739308
Ye-Yu <30603031+ye-yu@users.noreply.github.com>
2023-01-29 16:03:13 +0800

    Add test to construct primary key

--- src/database/utils/get-column-definition.util.spec.ts
+++ src/database/utils/get-column-definition.util.spec.ts
@@ -1,1 +1,17 @@
-describe("getColumnDefinition", () => {});
+import { Entity, PrimaryGeneratedColumn } from "typeorm";
+import { getColumnDefinition } from "./get-column-definition.util.js";
+
+describe("getColumnDefinition", () => {
+	it("should be able to construct primary key", () => {
+		@Entity()
+		class Model {
+			@PrimaryGeneratedColumn("increment")
+			primaryGenerated: number;
+		}
+
+		const tableColumn = getColumnDefinition(Model, "primaryGenerated");
+		expect(tableColumn.name).toEqual("primaryGenerated");
+		expect(tableColumn.isGenerated).toEqual(true);
+		expect(tableColumn.generationStrategy).toEqual("increment");
+	});
+});
