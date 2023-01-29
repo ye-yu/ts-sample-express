a69e70b484608acd2fb20b7ae84f4efba8751fd0
Ye-Yu <30603031+ye-yu@users.noreply.github.com>
2023-01-29 15:41:22 +0800

    Remove user model querying on app start

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,15 +1,11 @@
 import { DatabaseService } from "../database/database.service.js";
 import { LoggerService } from "../logger/logger.service.js";
-import { UserModel } from "../database/models/user.model.js";
 
 export class AppService {
 	async start(): Promise<void> {
 		const logger = LoggerService.for(this);
-		logger.debug("Initializing database...");
+		logger.info("Initializing database...");
 		await DatabaseService.start();
-		const userModel = DatabaseService.repo(UserModel);
-		const singleUser = await userModel.find();
-		console.log(singleUser);
 		return;
 	}
 }
