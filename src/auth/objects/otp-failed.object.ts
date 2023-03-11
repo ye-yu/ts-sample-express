1a75a087b5b6af227ebb6f6de7a73f70640f2ce9
Ye-Yu <30603031+ye-yu@users.noreply.github.com>
2023-03-11 20:37:04 +0800

    Add otp result objects

---
+++ src/auth/objects/otp-failed.object.ts
@@ -0,0 +1,6 @@
+export class OtpFailedObject {
+	success: false;
+	emailTo: string;
+	reason: string;
+	i18n: string;
+}
---
+++ src/auth/objects/otp-success.object.ts
@@ -0,0 +1,5 @@
+export class OtpSuccessObject {
+	success: true;
+	emailTo: string;
+	i18n: string;
+}
