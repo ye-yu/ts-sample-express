c4e4918b02f8301f58ec702916218824eeb4205a
Ye-Yu <30603031+ye-yu@users.noreply.github.com>
2023-03-11 20:36:47 +0800

    Add transaction to database service

--- src/database/database.service.ts
+++ src/database/database.service.ts
@@ -1,9 +1,10 @@
-import { DataSource, ObjectLiteral, Repository } from "typeorm";
+import { DataSource, EntityManager, ObjectLiteral, Repository } from "typeorm";
 import { LoggerService } from "../logger/logger.service.js";
 import { defaultDataSource } from "./data-sources/default.data-source.js";
+import { RepoGetterType } from "./types/repo-getter.type.js";
 import { ModelType } from "./types/model.type.js";
 
-export class DatabaseServiceImpl {
+export class DatabaseServiceImpl implements RepoGetterType {
 	defaultDataSource: DataSource;
 	async initDataSources(): Promise<void> {
 		this.defaultDataSource = defaultDataSource;
@@ -22,6 +23,22 @@ export class DatabaseServiceImpl {
 		const repo: any = this.defaultDataSource.getRepository(modelAny);
 		this.repoCache[modelAny] = repo;
 		return repo;
+	}
+
+	async transaction<T = any>(
+		callback: (
+			// TODO: see if entity manager is not needed and should we remove it
+			entityManager: EntityManager,
+			repoGetter: RepoGetterType
+		) => Promise<T>
+	): Promise<T> {
+		return await this.defaultDataSource.transaction(async (manager) => {
+			return await callback(manager, {
+				repo(model) {
+					return manager.getRepository(model);
+				},
+			});
+		});
 	}
 }
 
