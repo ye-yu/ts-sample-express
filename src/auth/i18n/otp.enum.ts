1faa2c22bdc12d9ff4afee7cc0d3b9a39bca4977
Ye-Yu <30603031+ye-yu@users.noreply.github.com>
2023-03-11 20:37:10 +0800

    Add otp i18n

---
+++ src/auth/i18n/otp.enum.ts
@@ -0,0 +1,5 @@
+export enum OtpI18nEnum {
+	otp_success = "otp_success",
+	otp_request_failed_already_requested = "otp_request_failed_already_requested",
+	otp_request_failed_system_error = "otp_request_failed_system_error",
+}
