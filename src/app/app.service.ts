e1077378a11fb09078516a1abace0c6314d8b278
Ye-Yu <30603031+ye-yu@users.noreply.github.com>
2023-03-12 21:38:21 +0800

    Init server service on app init

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,10 +1,13 @@
 import { DatabaseService } from "../database/database.service.js";
 import { LoggerService } from "../logger/logger.service.js";
+import { ServerService } from "../server/server.service.js";
 
 export class AppService {
 	async start(): Promise<void> {
 		const logger = LoggerService.for(this);
 		logger.info("Initializing database...");
+		await ServerService.start();
+		await ServerService.configureRoutes();
 		await DatabaseService.start();
 		return;
 	}
