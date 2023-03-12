7383959ce049c06e6f966f54763380d2930912a1
Ye-Yu <30603031+ye-yu@users.noreply.github.com>
2023-03-12 21:35:08 +0800

    Use pino pretty on development; Add calling function stack at logger

--- src/logger/logger.service.ts
+++ src/logger/logger.service.ts
@@ -1,6 +1,7 @@
 import pino from "pino";
 import HttpLogger, { pinoHttp } from "pino-http";
 import { ConfigService } from "../config/config.service.js";
+import { getCurrentCallingLocation } from "../common/utils/get-current-calling-location.util.js";
 
 export class LoggerServiceImpl implements pino.BaseLogger {
 	level: string;
@@ -10,6 +11,12 @@ export class LoggerServiceImpl implements pino.BaseLogger {
 			logger ??
 			pinoHttp({
 				level: ConfigService.app.logLevel,
+				transport:
+					ConfigService.app.env === "development"
+						? {
+								target: "pino-pretty",
+						  }
+						: undefined,
 				// to make it compatible with gcp log explorer
 				messageKey: "message",
 				errorKey: "message",
@@ -23,89 +30,61 @@ export class LoggerServiceImpl implements pino.BaseLogger {
 	}
 	fatal = (message: string, ...formatter: any[]): void => {
 		if (this.meta) {
-			Object.assign(this.meta, {
-				context: this.context,
-			});
+			Object.assign(this.meta, this.getContextObject());
 			this.logger.logger.fatal(this.meta, message, ...formatter);
 			this.meta = undefined;
 		} else {
-			this.logger.logger.fatal(
-				{ context: this.context },
-				message,
-				...formatter
-			);
+			this.logger.logger.fatal(this.getContextObject(), message, ...formatter);
 		}
 		return;
 	};
 	error = (message: string, ...formatter: any[]): void => {
 		if (this.meta) {
-			Object.assign(this.meta, {
-				context: this.context,
-			});
+			Object.assign(this.meta, this.getContextObject());
 			this.logger.logger.error(this.meta, message, ...formatter);
 			this.meta = undefined;
 		} else {
-			this.logger.logger.error(
-				{ context: this.context },
-				message,
-				...formatter
-			);
+			this.logger.logger.error(this.getContextObject(), message, ...formatter);
 		}
 		return;
 	};
 	warn = (message: string, ...formatter: any[]): void => {
 		if (this.meta) {
-			Object.assign(this.meta, {
-				context: this.context,
-			});
+			Object.assign(this.meta, this.getContextObject());
 			this.logger.logger.warn(this.meta, message, ...formatter);
 			this.meta = undefined;
 		} else {
-			this.logger.logger.warn({ context: this.context }, message, ...formatter);
+			this.logger.logger.warn(this.getContextObject(), message, ...formatter);
 		}
 		return;
 	};
 	info = (message: string, ...formatter: any[]): void => {
 		if (this.meta) {
-			Object.assign(this.meta, {
-				context: this.context,
-			});
+			Object.assign(this.meta, this.getContextObject());
 			this.logger.logger.info(this.meta, message, ...formatter);
 			this.meta = undefined;
 		} else {
-			this.logger.logger.info({ context: this.context }, message, ...formatter);
+			this.logger.logger.info(this.getContextObject(), message, ...formatter);
 		}
 		return;
 	};
 	debug = (message: string, ...formatter: any[]): void => {
 		if (this.meta) {
-			Object.assign(this.meta, {
-				context: this.context,
-			});
+			Object.assign(this.meta, this.getContextObject());
 			this.logger.logger.debug(this.meta, message, ...formatter);
 			this.meta = undefined;
 		} else {
-			this.logger.logger.debug(
-				{ context: this.context },
-				message,
-				...formatter
-			);
+			this.logger.logger.debug(this.getContextObject(), message, ...formatter);
 		}
 		return;
 	};
 	trace = (message: string, ...formatter: any[]): void => {
 		if (this.meta) {
-			Object.assign(this.meta, {
-				context: this.context,
-			});
+			Object.assign(this.meta, this.getContextObject());
 			this.logger.logger.trace(this.meta, message, ...formatter);
 			this.meta = undefined;
 		} else {
-			this.logger.logger.trace(
-				{ context: this.context },
-				message,
-				...formatter
-			);
+			this.logger.logger.trace(this.getContextObject(), message, ...formatter);
 		}
 		return;
 	};
@@ -114,6 +93,13 @@ export class LoggerServiceImpl implements pino.BaseLogger {
 	};
 
 	derivedLoggers: Record<string, LoggerServiceImpl> = {};
+	private getContextObject(): { context: string; at: string } {
+		return {
+			context: this.context,
+			at: getCurrentCallingLocation(),
+		};
+	}
+
 	for<T extends Object>(context: string | T): LoggerServiceImpl {
 		const stringContext =
 			typeof context === "string" ? context : context.constructor.name;
