f69576bdcab5ac3153b68eb7f94c1b563a6c90ef
Ye-Yu <30603031+ye-yu@users.noreply.github.com>
2023-03-12 18:58:08 +0800

    Add i18n key for otp verifications

--- src/auth/i18n/otp.enum.ts
+++ src/auth/i18n/otp.enum.ts
@@ -2,4 +2,10 @@ export enum OtpI18nEnum {
 	otp_success = "otp_success",
 	otp_request_failed_already_requested = "otp_request_failed_already_requested",
 	otp_request_failed_system_error = "otp_request_failed_system_error",
+	otp_verify_success = "otp_verify_success",
+	otp_verify_failed_user_not_found = "otp_verify_failed_user_not_found",
+	otp_verify_failed_incorrect = "otp_verify_failed_incorrect",
+	otp_verify_failed_not_requested = "otp_verify_failed_not_requested",
+	otp_verify_failed_expired = "otp_verify_failed_expired",
+	otp_verify_failed_system_error = "otp_verify_failed_system_error",
 }
