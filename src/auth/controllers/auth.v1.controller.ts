a488ee0731a0cd7423c0837d47c2b2f415049159
Ye-Yu <30603031+ye-yu@users.noreply.github.com>
2023-03-12 21:36:45 +0800

    Add basic auth controller for otp

---
+++ src/auth/controllers/auth.v1.controller.ts
@@ -0,0 +1,19 @@
+import { Post } from "../../server/decorators/post.decorator.js";
+import { Put } from "../../server/decorators/put.decorator.js";
+import { Request } from "../../server/types/request.type.js";
+import { AuthService } from "../auth.service.js";
+
+export class AuthController {
+	@Put("/api/v1/otp")
+	async requestOtp(req: Request) {
+		return await AuthService.requestOneTimePassword(req.body.email);
+	}
+
+	@Post("/api/v1/otp")
+	async validateOtp(req: Request) {
+		return await AuthService.verifyOneTimePassword(
+			req.body.email,
+			req.body.password
+		);
+	}
+}
