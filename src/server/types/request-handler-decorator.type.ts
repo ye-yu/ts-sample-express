204931d79e0193ae6237acd8f4e19fd8a6318c81
Ye-Yu <30603031+ye-yu@users.noreply.github.com>
2023-03-18 20:32:49 +0800

    Set request handler target type as prototype

--- src/server/types/request-handler-decorator.type.ts
+++ src/server/types/request-handler-decorator.type.ts
@@ -1,7 +1,8 @@
 import { RequestHandler } from "express";
+import { Prototype } from "../../common/types/prototype.type.js";
 
 export type RequestHandlerDecoratorType = <T extends RequestHandler>(
-	target: Object,
+	target: Prototype,
 	propertyKey: string | symbol,
 	descriptor: TypedPropertyDescriptor<T>
 ) => void | TypedPropertyDescriptor<T>;
