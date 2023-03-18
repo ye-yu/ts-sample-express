e71ff8947cc457aac9536a368e48773f8aa302c7
Ye-Yu <30603031+ye-yu@users.noreply.github.com>
2023-03-18 20:33:51 +0800

    Instantiate singleton on use decorator

--- src/server/types/request-handler-function.type.ts
+++ src/server/types/request-handler-function.type.ts
@@ -3,5 +3,6 @@ import { RequestHandler } from "./request-handler.type.js";
 export type RequestHandlerFunctionInfo = {
 	name: string;
 	handler: RequestHandler;
+	bindTo: any;
 	route: string;
 };
--- src/server/utils/create-method-decorator.util.ts
+++ src/server/utils/create-method-decorator.util.ts
@@ -1,19 +1,21 @@
 import { RequestHandlerFunctionInfo } from "../types/request-handler-function.type.js";
 import { RequestHandlerDecoratorType } from "../types/request-handler-decorator.type.js";
-import { RequestHandler } from "../types/request-handler.type.js";
+import { tryInstantiateSingleton } from "../../common/utils/try-instantiate-singleton.util.js";
 
 export function createMethodDecorator(
 	handlerList: Array<RequestHandlerFunctionInfo>
 ): (route: string) => RequestHandlerDecoratorType {
 	return (route) => {
-		return function <T extends RequestHandler>(
-			_: T,
+		return function (
+			target: any,
 			name: string | symbol,
 			desc: TypedPropertyDescriptor<any>
 		) {
+			const instance = tryInstantiateSingleton(target.constructor);
 			name = String(name);
 			handlerList.push({
 				handler: desc.value,
+				bindTo: instance,
 				name,
 				route,
 			});
